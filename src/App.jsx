051eedf7a2870eebb697af894c0550551c0d4d6c
exchyphen <exc.hyphen@gmail.com>
2024-03-15 16:57:17 -0700

    general scaffolding and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+/* images */
+import Logo from "./assets/images/logo.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <header className="header">
+        <img className="header__logo" src={Logo} alt="header logo img"></img>
+        <div>
+          <h1 className="text--header">A history of everything you copy</h1>
+          <p className="text--body">
+            Clipboard allows you to track and organize everything you copy.
+            Instantly access your clipboard on all your devices.
+          </p>
+        </div>
+        <div className="header__button-container">
+          <button className="button--ios">Download for iOS</button>
+          <button className="button--mac">Download for Mac</button>
+        </div>
+      </header>
+
+      <main className="main"></main>
+
+      <footer>
+        <p className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://github.com/exchyphen" target="_blank">
+            exc
+          </a>
+          .
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
